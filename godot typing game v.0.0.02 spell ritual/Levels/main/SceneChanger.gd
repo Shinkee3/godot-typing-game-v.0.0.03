160ccb774ea8eecab62cad9c764618abc7a7f7d0
Shinkeee <kaitlyn.katigbac@gmail.com>
2025-02-20 11:18:45 +0800

    Added three other levels to start making progress on other parts of the game
    -Added walls level 3-5
    -Added boss caster to level 3(mistake, should be on level 5 but w/e we can work with this)
    -

--- godot typing game v.0.0.02 spell ritual/Levels/main/SceneChanger.gd
+++ godot typing game v.0.0.02 spell ritual/Levels/main/SceneChanger.gd
@@ -4,7 +4,7 @@ extends SubViewportContainer
 
 @export var scene1 = "res://Levels/main/main_test_level.tscn"
 @export var scene2 = "res://Levels/arena_level.tscn"
-
+@export var scene3 = "res://Levels" #wait idt htis is useful
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
